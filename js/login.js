c69a0816abd15c3bd81b1a8d27d9cf6b2411d09e
franciscomen92 <110631370+franciscomen92@users.noreply.github.com>
2022-08-19 22:12:55 -0300

    JS Login
    Falta estilo

---
+++ js/login.js
@@ -0,0 +1,11 @@
+let usuario = document.getElementById("user");
+let contrasenia = document.getElementById("password");
+let boton = document.getElementById("button");
+
+boton.addEventListener("click", e=> {
+    if(usuario.value == "" && contrasenia.value == "") {
+        alert("Debes completar los campos requeridos...")
+    } else {
+        window.location.href = "index.html"
+    }
+})
