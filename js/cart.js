bcf21f9ee66b718b3b58d24e6a5ed9fe8c718ed7
franciscomen92 <110631370+franciscomen92@users.noreply.github.com>
2022-10-15 10:29:08 -0300

    Parte 3 entregable 5
    El subtotal cambia en tiempo real cuando se cambia el input

--- js/cart.js
+++ js/cart.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     getJSONData(userCart).then(result => {
         if (result.status == "ok") {
             userCartArray = result.data;
-            mostrarCarrito(userCartArray)
+            mostrarCarrito(userCartArray);
         } else {
             alert("Error al cargar los datos: " + result.data);
         }
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-
+/* Funcion que muestra el carrito */
 function mostrarCarrito(userCartArray) {
 for (const infoCart of userCartArray.articles) {
     let carrito = `
@@ -24,14 +24,26 @@ for (const infoCart of userCartArray.articles) {
     <p class="col"><img src="${infoCart.image}" class="imagenCarrito"></p>
     <p class="col">${infoCart.name}</p>
     <p class="col">${infoCart.currency}${infoCart.unitCost}</p>
-    <p class="col" ><input type="number" min="1" value="1" id="inputCantidad"></p>
-    <p class="col" id="subtotal"></input></p>
+    <p class="col" ><input type="number" min="1" value="1" id="inputCantidad"></input></p>
+    <p class="col" id="subtotal"></p>
     </div>
       `
     document.getElementById("productosAgregados").innerHTML += carrito;
+    document.getElementById("subtotal").innerHTML += infoCart.unitCost; /* Esto es para que al cargar la pagina aparezca el precio */
 
-    let subtotal = document.getElementById("inputCantidad").value * infoCart.unitCost;
+   /*  Funcion para actualizar el subtotal cuando se cambia el value del input */
+    let cantidad = document.getElementById("inputCantidad");
+    let precio = infoCart.unitCost;
+    let subtotal = document.getElementById("subtotal");
+
+
+    cantidad.addEventListener("input", updateSubtotal);
+    function updateSubtotal(e) {
+        subtotal.textContent = e.target.value * precio;
+    }
+
+   /*  let subtotal = document.getElementById("inputCantidad").value * infoCart.unitCost;
     
-    document.getElementById("subtotal").innerHTML = subtotal;
+    document.getElementById("subtotal").innerHTML = subtotal; */
 }
 }
